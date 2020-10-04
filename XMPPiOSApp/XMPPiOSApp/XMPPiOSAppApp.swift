85c92d8eebe3f1835444921ffdca37026801c45b
Jeremy Oddos <j.oddos@useradgents.com>
2020-10-04 19:15:05 +0200

    [General] - Created the app

---
+++ XMPPiOSApp/XMPPiOSApp/XMPPiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  XMPPiOSAppApp.swift
+//  XMPPiOSApp
+//
+//  Created by Jérémy Oddos on 04/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct XMPPiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
